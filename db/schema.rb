a453ef038792f8767e23620799b716371dd807d3
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-11-06 14:30:49 +0300

    modified schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_06_102546) do
+ActiveRecord::Schema.define(version: 2022_11_06_103329) do
 
   create_table "costumes", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,27 @@ ActiveRecord::Schema.define(version: 2022_11_06_102546) do
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "costumestores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "num_of_employees"
+    t.boolean "is_in_business"
+    t.datetime "opening_time"
+    t.datetime "closing_time"
+  end
+
+  create_table "hauntedhouses", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "theme"
+    t.float "price"
+    t.boolean "family_friendly"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
+    t.text "description"
   end
 
 end
